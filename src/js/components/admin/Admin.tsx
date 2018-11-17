2271f5135a4802c70952d48eee66ce21784690e9
Igor <gorbov18@gmail.com>
2018-11-17 21:46:33 +0400

    admin fetch data

--- src/js/components/admin/Admin.tsx
+++ src/js/components/admin/Admin.tsx
@@ -9,15 +9,19 @@ interface AdminProps {
 @inject('viewStore')
 @observer
 class Admin extends React.Component<AdminProps, any> {
+  componentDidMount() {
+    const { viewStore } = this.props;
+    viewStore.fetchData();
+  }
+
   render() {
     const { viewStore } = this.props;
-    const { players } = viewStore;
+    const { players, headToHeads } = viewStore;
     return (
       <div className="col-sm-8">
-        {players.length > 1 && <ManagePlayers />}
-
-        <ManageHeadToHeads />
-        <ManageGames />
+        <ManagePlayers />
+        {players.length > 1 && <ManageHeadToHeads />}
+        {headToHeads.length && <ManageGames />}
       </div>
     );
   }
