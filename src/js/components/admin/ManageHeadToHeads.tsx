9254911205b3933a18dbd0bd60ccced9c5894496
Igor <gorbov18@gmail.com>
2018-11-17 21:43:12 +0400

    handle HeadToHeads

--- src/js/components/admin/ManageHeadToHeads.tsx
+++ src/js/components/admin/ManageHeadToHeads.tsx
@@ -1,42 +1,55 @@
 import * as React from 'react';
-import AddHeadToHeadForm from './forms/AddHeadToHeadForm';
-import HeadToHeadRow from './forms/HeadToHeadRow';
+import { inject, observer } from 'mobx-react';
+import { AddHeadToHeadForm, HeadToHeadRow } from './forms';
 
-const ManageHeadToHeads = () => {
+interface ManageHeadToHeadsProps {
+  viewStore?: IViewStore;
+}
+
+const ManageHeadToHeads = (props: ManageHeadToHeadsProps) => {
+  const { viewStore } = props;
+  const { headToHeads } = viewStore;
   return (
     <div className="row">
       <h2>Manage Head To Heads</h2>
       <AddHeadToHeadForm />
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <h3 className="panel-title">Head To Heads</h3>
-        </div>
-        <div className="panel-body">
-          <div className="table-responsive">
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Title</th>
-                  <th>Player A</th>
-                  <th>Player B</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <HeadToHeadRow />
-                <tr>
-                  <td colSpan={5}>
-                    <p>Create your first head to head above.</p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+      {headToHeads.length ? (
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Head To Heads</h3>
+          </div>
+          <div className="panel-body">
+            <div className="table-responsive">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Player A</th>
+                    <th>Player B</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {headToHeads.length ? (
+                    headToHeads.map((headToHead, index) => (
+                      <HeadToHeadRow key={headToHead.key} headToHead={headToHead} index={index} />
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan={3}>
+                        <p>Create your first head to head above.</p>
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
 
-export default ManageHeadToHeads;
+export default inject('viewStore')(observer(ManageHeadToHeads));
--- src/js/components/admin/forms/AddHeadToHeadForm.tsx
+++ src/js/components/admin/forms/AddHeadToHeadForm.tsx
@@ -1,48 +1,146 @@
 import * as React from 'react';
+import { inject, observer } from 'mobx-react';
 
-class AddHeadToHeadForm extends React.Component<any, any> {
-    render() {
-        return (
-            <div className="panel panel-success">
-                <div className="panel-heading"><h3 className="panel-title">Add new Head To Head</h3></div>
-                <div className="panel-body">
-                    <form className="form">
-                        <div className="row">
-                            <div className="col-sm-12 col-md-12">
-                                <div className="form-group">
-                                    <label htmlFor="headToHeadName">Title</label>
-                                    <input type="text" className="form-control" id="headToHeadName" name="headToHeadName" placeholder="VS Your Mate" />
-                                </div>
-                            </div>
-                            <div className="col-sm-12 col-md-6">
-                                <div className="form-group">
-                                    <label htmlFor="playerA">Your Name</label>
-                                    <select className="form-control" id="playerA" name="playerA"> 
-                                        <option value=''>Select a player</option>
-                                        <option value='a'>Player A</option>
-                                        <option value='b'>Player B</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-sm-12 col-md-6">
-                                <div className="form-group">
-                                    <label htmlFor="playerB">Your Friend</label>
-                                    <select className="form-control" id="playerB" name="playerB"> 
-                                        <option value=''>Select a player</option>
-                                        <option value='a'>Player A</option>
-                                        <option value='b'>Player B</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="col-sm-12">
-                                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        );
+interface AddHeadToHeadFormProps {
+  viewStore?: IViewStore;
+}
+
+interface AddHeadToHeadFormState {
+  headToHeadName: string;
+  playerA: string;
+  playerB: string;
+}
+
+@inject('viewStore')
+@observer
+class AddHeadToHeadForm extends React.Component<AddHeadToHeadFormProps, AddHeadToHeadFormState> {
+  state = {
+    headToHeadName: '',
+    playerA: '',
+    playerB: '',
+  };
+
+  updateState = (key: string, value: string) => {
+    this.setState(prevState => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { name, value }: { name: string; value: string } = e.target;
+    this.updateState(name, value);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { headToHeadName, playerA, playerB } = this.state;
+    const { viewStore } = this.props;
+
+    viewStore.addHeadToHead(headToHeadName, playerA, playerB);
+
+    if (
+      headToHeadName &&
+      headToHeadName.trim().length &&
+      playerA &&
+      playerA.trim().length &&
+      playerB &&
+      playerB.trim().length
+    ) {
+      this.setState({
+        headToHeadName: '',
+        playerA: '',
+        playerB: '',
+      });
     }
+  };
+
+  render() {
+    const { headToHeadName, playerA, playerB } = this.state;
+    const { viewStore } = this.props;
+    const { players } = viewStore;
+    return (
+      <div className="panel panel-success">
+        <div className="panel-heading">
+          <h3 className="panel-title">Add new Head To Head</h3>
+        </div>
+        <div className="panel-body">
+          <form className="form" onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="col-sm-12 col-md-12">
+                <div className="form-group">
+                  <label htmlFor="headToHeadName">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="headToHeadName"
+                    name="headToHeadName"
+                    placeholder="VS Your Mate"
+                    onChange={this.handleInputChange}
+                    value={headToHeadName}
+                  />
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <div className="form-group">
+                  <label htmlFor="playerA">Your Name</label>
+                  <select
+                    className="form-control"
+                    id="playerA"
+                    name="playerA"
+                    onChange={this.handleInputChange}
+                    value={playerA}
+                  >
+                    <option value="">Select a player</option>
+                    {players.length &&
+                      players.map(player => {
+                        const { key, name } = player;
+                        return (
+                          <option key={key} value={key}>
+                            {name}
+                          </option>
+                        );
+                      })}
+                  </select>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-6">
+                <div className="form-group">
+                  <label htmlFor="playerB">Your Friend</label>
+                  <select
+                    className="form-control"
+                    id="playerB"
+                    name="playerB"
+                    onChange={this.handleInputChange}
+                    value={playerB}
+                  >
+                    <option value="">Select a player</option>
+                    {players.length &&
+                      players.map(player => {
+                        const { key, name } = player;
+                        return (
+                          <option key={key} value={key}>
+                            {name}
+                          </option>
+                        );
+                      })}
+                  </select>
+                </div>
+              </div>
+              <div className="col-sm-12">
+                <button type="submit" className="btn btn-primary btn-block">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AddHeadToHeadForm;
